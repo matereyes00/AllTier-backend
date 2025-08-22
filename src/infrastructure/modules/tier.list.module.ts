8c8b895730fc5abb54f362edd49fca79f02cda0f
Martina Therese Reyes <mreyes@ubx.ph>
2025-08-22 10:29:16 +0800

    Imported item to the tier list module

--- src/infrastructure/modules/tier.list.module.ts
+++ src/infrastructure/modules/tier.list.module.ts
@@ -5,9 +5,10 @@ import { TierListController } from '../http/controllers/tier.list.controller';
 import { TierListService } from '../../application/services/tier.list.service';
 import { TierListRepository } from '../../infrastructure/database/repositories/tier.list.repository';
 import { AuthModule } from './auth.module';
+import { Item } from 'src/domain/entities/item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TierList]), AuthModule],
+  imports: [TypeOrmModule.forFeature([TierList, Item]), AuthModule],
   controllers: [TierListController],
   providers: [TierListService, TierListRepository],
 })
