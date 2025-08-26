9e2d6be6e76271112179b9d8a31f8d4b91c599a1
Martina Therese Reyes <mreyes@ubx.ph>
2025-08-26 15:03:01 +0800

    added tier list type to the tier list entity with its enum

--- src/domain/entities/tier.list.entity.ts
+++ src/domain/entities/tier.list.entity.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from 'typeorm';
 import { User } from './user.entity';
 import { Item } from './item.entity';
+import { TierListType } from '../enum/tier.list.enum';
 
 @Entity()
 export class TierList {
@@ -9,6 +10,9 @@ export class TierList {
 
   @Column({ unique: true })
   tierListName: string;
+
+  @Column({ type: 'enum', enum: TierListType })
+  tierListType: string;
 
   @ManyToOne(() => User, (user) => user.tierLists)
   user: User;
---
+++ src/domain/enum/tier.list.enum.ts
@@ -0,0 +1,4 @@
+export enum TierListType{
+    TOURNAMENT = 'tournament',
+    RATING='rating'
+}
